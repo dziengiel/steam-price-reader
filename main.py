ee002b252a0c608c26243743dc0d69ddaea327e8
dziegiel <jakubdzieg33@gmail.com>
2024-08-17 12:32:10 +0200

    added name finding

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ r = requests.get(link)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
+name = soup.find('div', class_='apphub_AppName')
+
 s = soup.find('div', class_='discount_final_price')
 
 if s == None:
@@ -14,6 +16,11 @@ if s == None:
 
 s = s.prettify()
 
+name = name.prettify()
+
+name = name.splitlines()[1]
+
 price = s.splitlines()[1]
 
-print(price)
+print(f'Name: {name}\n'
+      f'Price:{price}')
